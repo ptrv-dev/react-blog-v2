e2273c157a9403497a10e5fd5aed22ab031c8ee6
ptrv-dev <petrov99119@gmail.com>
2022-11-23 11:46:01 +0200

    Latest News

---
+++ src/components/LatestNews/News.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import style from './News.module.scss';
+
+interface NewsProps {
+  _id: string;
+  image: string;
+  title: string;
+  text: string;
+}
+
+const News: React.FC<NewsProps> = ({ _id, image, title, text }) => {
+  return (
+    <div className={style.root}>
+      <img src="./assets/img/news/1.jpg" alt={title} />
+      <h4>{title}</h4>
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default News;
--- src/components/LatestNews/index.tsx
+++ src/components/LatestNews/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 import style from './LatestNews.module.scss';
+import News from './News';
 
 const LatestNews: React.FC = () => {
-  return <div>LatestNews</div>;
+  return (
+    <div className={style.root}>
+      <h3>Последние новости:</h3>
+      <div className={style.column}>
+        {[...Array(3)].map((item, idx) => (
+          <News
+            key={idx}
+            _id="123"
+            image="1.jpg"
+            title="Какой-то заголовок для новости"
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quasi temporibus quidem, pariatur molestiae autem."
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LatestNews;
